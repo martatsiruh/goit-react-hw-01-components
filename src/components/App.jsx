ce8a628a0b120299b3a0a3be1c1408eda20f304b
Marta Tsiruh <martatsiruch@gmail.com>
2022-05-03 01:20:13 +0200

    add user profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,17 @@ import Section from './Section/Section';
 import user from '../data/user.json';
 import Profile from './Profile/Profile';
 
+import statisticalData from '../data/statistical-data.json';
+import Statistics from './Statistics/Statistics';
+
+import friends from '../data/friends.json';
+import FriendList from './FriendList/FriendList';
+
+import transactions from '../data/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+
+
 export const App = () => {
   return (
     <div>
@@ -14,7 +25,18 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-    </Section>
+      </Section>
+      
+
+      <Statistics title="Upload stats" stats={statisticalData}></Statistics>
+      <Statistics stats={statisticalData} />
+      
+      <Section>
+        <FriendList friends={friends} />
+      </Section>
+
+      <TransactionHistory items={transactions} />
+
     </div>
   );
 };
